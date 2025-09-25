63592ac66aa7cffdf18fd5f0ecd68b76799eb2bc
Angélica Shigematsu <angelica_shigematsu@outlook.com>
2025-09-25 11:30:41 -0300

    feature: add purchase order

--- pedido/views.py
+++ pedido/views.py
@@ -1,13 +1,114 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views import View
+from django.contrib import messages
+from produto.models import Varicao
+from . import models
+from utils import utils
+from django.http import HttpResponse
 # Create your views here.
 
 class Pagar(View):
-  pass
+  template_name = 'pedido/pagar.html'
+
+  def get(self, *args, **kwargs):
+    if not self.request.user.is_authenticated:
+      messages.error(
+        self.request,
+        'Você precisa fazer login'
+      )
+      return redirect('perfil:criar')
+    
+    if not self.request.session.get('carrinho'):
+      messages.error(
+        self.request,
+        'Carrinho vazio'
+      )
+      return redirect('produto:lista')
+    
+    carrinho = self.request.session.get('carrinho')
+    carrinho_variacao_ids = [v for v in carrinho ]
+    bd_variacoes = list(
+      Varicao.objects.select_related('produto')
+      .filter(id__in=carrinho_variacao_ids)
+    )
+
+    for variacao in bd_variacoes:
+      vid = str(variacao.id)
+
+      estoque = variacao.estoque
+      qtd_carrinho = carrinho[vid]['quantidade']
+      preco_unitario = carrinho[vid]['preco_quantitativo']
+      preco_unitario_promocional = carrinho[vid]['preco_unitario_promocional']
+     
+      error_msg_estoque = ''
+
+      if estoque < qtd_carrinho:
+        carrinho[vid]['quantidade'] = estoque
+        carrinho[vid]['preco_quantitativo'] = estoque * preco_unitario
+        carrinho[vid]['preco_unitario_promocional'] = estoque * \
+        preco_unitario_promocional
+
+        error_msg_estoque = 'Estoque insuficiente para alguns produtos do seu carrinho'
+        'Reduzimos a quantidade desses produtos. Por favor, verifique'
+        'quais produtos foram afetados a seguir'
+
+        messages.error(
+          self.request,
+          error_msg_estoque
+        )
+        self.request.session.save()
+        return redirect('produto:carrinho')
+        
+    qtd_total_carrinho = utils.cart_total_qtd(carrinho)
+    valor_total_carrinho = utils.cart_total_qtd(carrinho)    
+    
+    pedido = models.Pedido(
+      usuario=self.request.user,
+      total=valor_total_carrinho,
+      qtd_total=qtd_total_carrinho,
+      status='C'
+    )
+
+    pedido.save()
+
+    models.ItemPedido.objects.bulk_create(
+      [
+        models.ItemPedido(
+          pedido=pedido,
+          produto=v['produto_nome'],
+          produto_id=v['produto_id'],
+          variacao=v['variacao_nome'],
+          variacao_id=v['variacao_id'],
+          preco=v['preco_quantitativo'],
+          preco_promocional=v['preco_quantitativo_promocional'],
+          quantidade=v['quantidade'],
+          imagem=v['imagem'],
+        ) for v in carrinho.values()
+      ]
+    )
+
+    contexto = {
+      'qtd_total_carrinho': qtd_total_carrinho,
+      'valor_total_carrinho': valor_total_carrinho
+    }
+
+    del self.request.session['carrinho']
+
+    # renderiza = render(
+    #   self.request,
+    #   self.template_name,
+    #   contexto
+    # )
+
+    return redirect('pedido:lista')
 
 class SalvarPedido(View):
   pass
 
 class Detalhe(View):
   pass
+
+class Lista(View):
+  def get(self, *args, **kwargs):
+    return HttpResponse('LIsta')
